33bcd1c80f7976f6430fecf9551418dcf0d63226
Arnie <arnie752@gmail.com>
2023-10-17 01:32:18 +0300

    Updated the appearance of the messages and messages now have authors

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,13 +20,11 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -36,11 +33,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 class Message(
+    var id: Int?,
     var author: String,
     var message: String
 ) {}
@@ -56,7 +56,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
-                        MessageList(messages = messageList)
+                        MessageList(messages = messageList, user_id = 1)
                     }
                 }
             }
@@ -65,24 +65,36 @@ class MainActivity : ComponentActivity() {
 }
 
 var messageList: List<Message> = listOf(
-    Message(author = "John Doe", message = "Lorem Ipsum"),
-    Message(author = "John Doe", message = "Lorem Ipsum"),
-    Message(author = "John Doe", message = "Lorem Ipsum"),
-    Message(author = "John Doe", message = "Lorem Ipsum"),
-    Message(author = "John Doe", message = "Lorem Ipsum"),
-    Message(author = "John Doe", message = "Lorem Ipsum"),
-    Message(author = "John Doe", message = "Lorem Ipsum"),
-    Message(author = "John Doe", message = "Lorem Ipsum"),
-    Message(author = "John Doe", message = "Lorem Ipsum"),
-    Message(author = "John Doe", message = "Lorem Ipsum"),
-    Message(author = "John Doe", message = "Lorem Ipsum"),
-    Message(author = "John Doe", message = "Lorem Ipsum"),
-    Message(author = "John Doe", message = "Lorem Ipsum"),
+    Message(id = 1, author = "John Doe", message = "Lorem Ipsum"),
+    Message(id = 1, author = "John Doe", message = "Lorem Ipsum"),
+    Message(id = 1, author = "John Doe", message = "Lorem Ipsum"),
+    Message(
+        id = 1,
+        author = "John Doe",
+        message = "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+    ),
+    Message(id = 2, author = "Jane Doe", message = "Lorem Ipsum"),
+    Message(id = 2, author = "Jane Doe", message = "Lorem Ipsum"),
+    Message(id = 2, author = "Jane Doe", message = "Lorem Ipsum"),
+    Message(
+        id = 1,
+        author = "John Doe",
+        message = "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+    ),
+    Message(id = 2, author = "Jane Doe", message = "Lorem Ipsum"),
+    Message(id = 1, author = "John Doe", message = "Lorem Ipsum"),
+    Message(id = 2, author = "Jane Doe", message = "Lorem Ipsum"),
+    Message(
+        id = 1,
+        author = "John Doe",
+        message = "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+    ),
+    Message(id = 2, author = "Jane Doe", message = "Lorem Ipsum"),
 )
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun MessageList(messages: List<Message>) {
+fun MessageList(messages: List<Message>, user_id: Int) {
     ComposeTutorialTheme {
         Surface {
             LazyColumn(
@@ -106,7 +118,7 @@ fun MessageList(messages: List<Message>) {
                     }
                 }
                 items(messages) { message ->
-                    MessageCard(message = message)
+                    MessageCard(message = message, author = user_id)
                 }
             }
         }
@@ -114,38 +126,60 @@ fun MessageList(messages: List<Message>) {
 }
 
 @Composable
-fun MessageCard(message: Message) {
+fun MessageCard(message: Message, author: Int) {
     ComposeTutorialTheme {
-        Surface {
+        Surface(
+
+        ) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(all = 8.dp),
                 verticalAlignment = Alignment.Top,
-                horizontalArrangement = Arrangement.Start
+                horizontalArrangement = if (message.id == author) Arrangement.Start else Arrangement.End,
             ) {
-                Image(
-                    painter = painterResource(R.drawable.ic_launcher_background),
-                    contentDescription = "Contact profile picture",
-                    modifier = Modifier
-                        // Set image size to 40 dp
-                        .size(46.dp)
-                        // Clip image to be shaped as a circle
-                        .clip(CircleShape)
-                        .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
-                )
-
                 // Add a horizontal space between the image and the column
-                Spacer(modifier = Modifier.width(8.dp))
                 var isExpanded by remember { mutableStateOf(false) }
+                if (message.id == author) {
+                    Image(
+                        painter = painterResource(R.drawable.ic_launcher_background),
+                        contentDescription = "Contact profile picture",
+                        modifier = Modifier
+                            // Set image size to 40 dp
+                            .size(46.dp)
+                            // Clip image to be shaped as a circle
+                            .clip(CircleShape)
+                            .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
                 Column(
                     verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = if (message.id == author) Alignment.Start else Alignment.End,
                     modifier = Modifier.clickable { isExpanded = !isExpanded }
                 ) {
-                    Text(text = message.author)
+                    Text(text = message.author, fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.onBackground)
                     Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = message.message, maxLines = if (isExpanded) Int.MAX_VALUE else 1)
+                    Text(text = message.message,
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
                 }
+
+                if (message.id != author) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Image(
+                        painter = painterResource(R.drawable.ic_launcher_background),
+                        contentDescription = "Contact profile picture",
+                        modifier = Modifier
+                            // Set image size to 40 dp
+                            .size(46.dp)
+                            // Clip image to be shaped as a circle
+                            .clip(CircleShape)
+                            .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                    )
+                }
+
             }
         }
     }
